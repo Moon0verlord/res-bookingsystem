498beb64f7290fad5ade9f35b6a4e11bb497ac09
Robin Otterspeer <robin.otterspeer2005@gmail.com>
2023-02-14 16:06:10 +0100

    add

--- Mini Project/RPG/Monster.cs
+++ Mini Project/RPG/Monster.cs
@@ -4,5 +4,22 @@ public class Monster
 {
     int ID;
     string Name;
-    int CurrentHP;
+    string NamePlural;
+    int MaximumDamage;
+    int RewardExperience;
+    int RewardGold;
+    List Loot;
+    int CurrentHitPoints;
+    public Monster(int id, string name,
+        string nameplural, int maximumDamage, int rewardExperience, int rewardGold, List loot, int currentHitPoints)
+    {
+        ID = id;
+        Name = name;
+        NamePlural = nameplural;
+        MaximumDamage = maximumDamage;
+        RewardExperience = rewardExperience;
+        RewardGold = rewardGold;
+        Loot = loot;
+        CurrentHitPoints = currentHitPoints;
+    }
 }
