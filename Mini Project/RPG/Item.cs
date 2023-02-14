40949a7e472340ae49365653ababbae8bb28640e
WarMartijn <1058740@hr.nl>
2023-02-14 15:52:53 +0100

    Hello

--- Mini Project/RPG/Item.cs
+++ Mini Project/RPG/Item.cs
@@ -4,4 +4,12 @@ public class Item
 {
     public int ID;
     public string Name;
+    public string NamePlural;
+
+    public Item(int id,string name,string namePlural)
+    {
+        ID = id;
+        Name = name;
+        NamePlural = namePlural;
+    }
 }
