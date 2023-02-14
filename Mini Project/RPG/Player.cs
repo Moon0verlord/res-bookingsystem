a459a8857dbadee2e7ace2a4e2e0e0c34937f4a2
Moon0verlord <114489960+Moon0verlord@users.noreply.github.com>
2023-02-14 23:07:19 +0100

    Finished Constructor

--- Mini Project/RPG/Player.cs
+++ Mini Project/RPG/Player.cs
@@ -4,15 +4,22 @@ public class Player
 {
     string Name;
     int MaxHP;
+    int CurrentHP;
     int gold;
     int XP;
     int level;
     Weapon CurrentWeapon;
     Location CurrentLocation;
     
-    public Player(string name)
+    public Player(string Name, int MaxHP, int CurrentHP, int gold, int XP, int level, Weapon CurrentWeapon, Location CurrentLocation)
     {
-        Name = name;
+        this.Name = Name;
         MaxHP = 100;
+        this.CurrentHP = CurrentHP;
+        this.gold = gold;
+        this.XP = XP;
+        this.level = level;
+        this.CurrentWeapon = CurrentWeapon;
+        this.CurrentLocation = CurrentLocation;
     }
 }
