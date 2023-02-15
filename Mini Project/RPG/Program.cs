1eac22fc6c2e27fc916d013e1e3e75e46beb2b83
WarMartijn <1058740@hr.nl>
2023-02-15 13:58:19 +0100

    Fighting

--- Mini Project/RPG/Program.cs
+++ Mini Project/RPG/Program.cs
@@ -4,15 +4,14 @@ public class Program
 {
     static void Main(string[] args)
     {
-        // Main loop
-        bool boolval = true;
+        var boolval = true;
         Console.WriteLine("The people in your town are being terrorized by giant spiders.\n" +
                           "You decide to do what you can to help.");
         Console.WriteLine("But first, who are you?");
         Console.Write("Enter your name: ");
         string name = Console.ReadLine()!;
         Player.CurrentWeapon = World.WeaponByID(World.WEAPON_ID_RUSTY_SWORD);
-        Player.CurrentLocation = World.LocationByID(World.LOCATION_ID_HOME);
+        Player.CurrentLocation = World.LocationByID(World.LOCATION_ID_SPIDER_FIELD);
         Player player = new Player(name,10,10,10,
         0,1, Player.CurrentWeapon,Player.CurrentLocation);
         while (boolval)
@@ -28,17 +27,26 @@ public class Program
                         Console.WriteLine($"Name: {name}.\nMax hp: {Player.MaxHP}.\n" +
                                           $"Current hp: {Player.CurrentHP}.\nGold: {Player.Gold}."+
                         $"\nXp: {Player.XP}\nLevel: {Player.Level}.\nCurrent Weapon: {Player.CurrentWeapon.Name}."+
-                                          $"\nCurrent Location: {Player.CurrentLocation.Name}.");
+                                          $"\nCurrent Location: {Player.CurrentLocation.Description}.");
                         break;
                     case 2:
                         Console.WriteLine("Where would you like to go?");
-                        Console.WriteLine($"You are at: {Player.CurrentLocation.Name}.\n{Player.CurrentLocation.Description}." +
-                                          $"\nFrom here you can go to:");
-                        Console.WriteLine(Player.CurrentLocation.Compass());
-                        Console.WriteLine(Player.CurrentLocation.Map());
+                        Console.WriteLine($"You are at: {Player.CurrentLocation.Name}." +
+                                          $"\nFrom here you can go:");
+                        //compass//
+                        Console.WriteLine(Player.CurrentLocation.LocationToNorth.Name);
+                        Console.WriteLine(Player.CurrentLocation.LocationToEast);
                         break;
                     case 3:
-                        Console.WriteLine(3);
+                        if (Player.CurrentLocation.MonsterLivingHere != null)
+                        {
+                            Console.WriteLine($"The {Player.CurrentLocation.MonsterLivingHere.Name} attacks!");
+                            fight();
+                        }
+                        else
+                        {
+                            Console.WriteLine("There seems to be nothing here to fight..");
+                        }
                         break;
                     case 4:
                         Console.WriteLine("Goodbye!");
@@ -53,6 +61,47 @@ public class Program
             {
                 Console.WriteLine("Invalid input. Please enter a valid option.\n");
             }
+            
+        }
+    }
+
+    public static void fight()
+    {
+        Random rnd = new Random();
+        var hitChanceRand = rnd.Next(1, 6);
+        var monster = Player.CurrentLocation.MonsterLivingHere;
+        Console.WriteLine($"You have: {Player.CurrentHP} Hp");
+        Console.WriteLine($"The {monster.Name} has: {monster.CurrentHitPoints} Hp");
+        while (Player.CurrentHP > 0)
+        {
+            var damage = rnd.Next(Player.CurrentWeapon.MinimumDamage, Player.CurrentWeapon.MaximumDamage);
+            var monsterDamage = rnd.Next(1, Monster.MaximumDamage);
+            if (hitChanceRand / 2 >= 1)
+            {
+                Console.WriteLine($"You hit the {monster.Name}!");
+                
+                monster.CurrentHitPoints = monster.CurrentHitPoints- damage;
+                Console.WriteLine(monster.CurrentHitPoints-damage);
+                if (monster.CurrentHitPoints <= 0)
+                {
+                    Console.WriteLine("You won");
+                    //add item to inventory//
+                    break;
+                }
+                Console.WriteLine($"The {monster.Name} has: 0 Hp");
+            }
+            else
+            {
+                Console.WriteLine("You missed haha");
+                Console.WriteLine($"The {monster.Name} has: {monster.CurrentHitPoints} Hp");
+            }
+            Console.WriteLine($"The {monster.Name} hits you!");
+            Player.CurrentHP = Player.CurrentHP - monsterDamage;
+            Console.WriteLine($"You have: {Player.CurrentHP} Hp");
+        }
+        if (Player.CurrentHP <= 0)
+        {
+            Console.WriteLine("You sadly passed away:(");
         }
     }
 }
