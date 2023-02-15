0a9e62022db476a1d06ffa90e33b1667ae7e016b
WarMartijn <1058740@hr.nl>
2023-02-15 11:03:35 +0100

    Started on main

--- Mini Project/RPG/Program.cs
+++ Mini Project/RPG/Program.cs
@@ -4,6 +4,7 @@ public class Program
 {
     static void Main(string[] args)
     {
-        
+        Console.WriteLine("The people in your town are being terrorized by giant spiders.\n" +
+                          "You decide to do what you can to help.");
     }
 }
