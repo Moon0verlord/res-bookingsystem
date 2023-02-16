742a0d4a42c8d4463690cf16a48a1aec4018d1d3
Moon0verlord <114489960+Moon0verlord@users.noreply.github.com>
2023-02-16 13:39:10 +0100

    Removed Function call

--- Mini Project/RPG/Program.cs
+++ Mini Project/RPG/Program.cs
@@ -14,8 +14,7 @@ public class Program
         Player.CurrentLocation = World.LocationByID(World.LOCATION_ID_HOME);
         Player player = new Player(name,10,10,10,
         0,1, Player.CurrentWeapon,Player.CurrentLocation);
-        Player.ViewInventory();
-        
+
         while (boolval)
         {
             try
