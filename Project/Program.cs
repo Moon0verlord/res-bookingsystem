150fb82ee0ad17a08c988515d30912ce2f9e1c34
WarMartijn <1058740@hr.nl>
2023-03-14 13:49:34 +0100

    Added a main

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,3 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to this amazing program");
-Menu.Start();
+﻿public class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to this amazing program");
+        Menu.Start();
+    }
+}
