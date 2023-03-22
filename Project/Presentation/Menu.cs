1192fa875493788963d62e666be849d7d4249ac6
Moon0verlord <114489960+Moon0verlord@users.noreply.github.com>
2023-03-22 14:28:19 +0100

    added to menu

---
+++ Project/Presentation/Menu.cs
@@ -0,0 +1,31 @@
+﻿namespace Project.Presentation;
+
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+public class Menu
+{
+    public List<Course> Courses { get; set; }
+}
+
+public class Course
+{
+    public string Appetizer { get; set; }
+    public string Soup { get; set; }
+    public string Entree { get; set; }
+    public string Dessert { get; set; }
+}
+
+public void ReadAndDisplayJson(string json)
+{
+    StreamReader reader = new StreamReader("Dishes.json");
+    string jsonString = reader.ReadToEnd();
+    reader.Close();
+    var Menus = JsonConvert.DeserializeObject<List<Menu>>(json);
+    foreach (var item in Menus)
+    {
+        Console.WriteLine("Menu");
+        Console.WriteLine(item.Courses);
+    }
+}
