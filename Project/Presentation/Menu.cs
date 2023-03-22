d9f5dc3944507c4fbbf3364299e7b1ef9ff53adb
Rokimajo <robinvliegtvandaag@gmail.com>
2023-03-22 12:42:23 +0100

    Merged MenuTesting into Main

--- Project/Presentation/Menu.cs
+++ Project/Presentation/Menu.cs
@@ -1,73 +1,57 @@
+using System.Data;
+
 static class Menu
 {
+    static private MenuLogic _myMenu = new MenuLogic();
 
     //This shows the menu. You can call back to this method to show the menu again
     //after another presentation method is completed.
     //You could edit this to show different menus depending on the user's role
     public static void Start(AccountModel acc = null)
     {
-        while (true)
+        if (acc == null)
         {
-
-            if (acc == null || acc.loggedIn == false)
+            while (true)
             {
-                Console.WriteLine("[1] to login");
-            }
-            else
-            {
-                Console.WriteLine("[1] to logout");
-            }
-
-            Console.WriteLine("[2] to get the restaurant information");
-            Console.WriteLine("[3] to get Schedule/ availability");
-            Console.WriteLine("[4] to view the current menu");
-            if (acc == null || acc.loggedIn == false)
-            {
-                Console.WriteLine("[5] to make a reservation with your email");
-            }
-            else
-            {
-                Console.WriteLine("[5] to make a reservation");
-                
-            }
-
-            Console.WriteLine("[6] to view the menu");
-            Console.WriteLine("[q] to leave");
-        
-        string input = Console.ReadLine()!;
-        
-            switch (input)
-            {
-                case "1":
-                    if (acc==null||acc.loggedIn == false)
-                    {
+                string[] options = { "Log in", "Information", "Schedule", "View current menu", "Make reservation with e-mail"};
+                string prompt = "\nMenu:";
+                int input = _myMenu.RunMenu(options, prompt);
+                switch (input)
+                {
+                    case 0:
                         UserLogin.Start();
                         break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Succesfully logged out.");
-                        Start(null);
-                    }
-                    break;
-                case "2":
-                    break;
-                case "3":
-                    break;
-                case "4":
-                    break;
-                case "5":
-                    break;
-                case "6":
-                    break;
-                case "q":
-                    Environment.Exit(0);
-                    break;
-                default:
+                    case 1:
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        else if (acc != null && acc.loggedIn)
+        {
+            while (true)
+            {
+                string[] options = { "Log out", "Information", "Schedule", "View current menu", "Make reservation"};
+                string prompt = $"\nWelcome {acc.FullName}.:";
+                int input = _myMenu.RunMenu(options, prompt);
+                switch (input)
                 {
-                    Console.WriteLine("Invalid input");
-                    Start();
-                    break;
+                    case 0:
+                        if  (acc.loggedIn == true)
+                        {
+                            Menu.Start();
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Already logged out");
+                        }
+                        break;
+                    case 1:
+                        break;
+                    default:
+                        break;
                 }
             }
         }
--- Project/Presentation/UserLogin.cs
+++ Project/Presentation/UserLogin.cs
@@ -1,39 +1,56 @@
 static class UserLogin
 {
     static private AccountsLogic accountsLogic = new AccountsLogic();
-
-
+    static private MenuLogic myMenu = new MenuLogic();
     public static void Start()
     {
-        Console.WriteLine("Welcome to the login page");
-        Console.WriteLine("Please enter your email address");
-        string email = Console.ReadLine()!;
-        Console.WriteLine("Please enter your password");
-        string password = Console.ReadLine()!;
-        AccountModel acc = accountsLogic.CheckLogin(email, password);
-        if (acc != null)
+        string userEmail = null;
+        string userPassword = null;
+        string prompt = "Welcome to the log in menu.\n";
+        string[] options = { "Enter e-mail", "Enter password", "Quit" };
+        while (true)
         {
-            //Password verification
-            while (true)
+            int selectedIndex = myMenu.RunMenu(options, prompt);
+            switch (selectedIndex)
             {
-                Console.WriteLine("Verify Password");
-                var VPassword = Console.ReadLine()!;
-                if (VPassword == password)
+                case 0:
+                    Console.Clear();
+                    Console.Write("Enter your e-mail: ");
+                    userEmail = Console.ReadLine()!;
+                    options[0] += $": {userEmail}";
+                    break;
+                case 1:
+                    Console.Clear();
+                    Console.Write("Enter your password: ");
+                    userPassword = Console.ReadLine()!;
+                    options[1] += $": {userPassword}";
+                    break;
+                case 2:
+                    Menu.Start();
+                    break;
+            }
+
+            if (userEmail != null && userPassword != null )
+            { 
+                AccountModel acc = accountsLogic.CheckLogin(userEmail, userPassword);
+                if (acc != null)
                 {
+                    Console.Clear();
                     Console.WriteLine("Welcome back " + acc.FullName);
                     Console.WriteLine("Your email number is " + acc.EmailAddress);
                     acc.loggedIn = true;
+                    Thread.Sleep(2000);
                     Menu.Start(acc);
+                    break;
                 }
                 else
                 {
-                    Console.WriteLine("Incorrect input");
+                    Console.WriteLine("No account found with that email and password");
+                    Thread.Sleep(2000);
+                    Menu.Start();
+                    break;
                 }
             }
-        }
-        else
-        {
-            Console.WriteLine("No account found with that email and password");
         }
     }
 }
