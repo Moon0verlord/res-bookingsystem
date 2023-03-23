575c7ecb66caf0350265b1b454c57b104fa95a05
WarMartijn <1058740@hr.nl>
2023-03-23 15:58:29 +0100

    Reduced the size of the months available in ReservationsLogic.cs

--- Project/Logic/ReservationsLogic.cs
+++ Project/Logic/ReservationsLogic.cs
@@ -26,80 +26,23 @@ class ReservationsLogic
     }
     public void ReservationsMenu()
     {
-        var options = new string[13 - cur_month + 1];
+        var options = new string[2];
             var counter = 0;
-            for (int i = cur_month; i <= 12; i++)
-            {
-                if (i == 12)
-                {
-                    options[counter] = DateTimeFormatInfo.CurrentInfo.GetMonthName(i);
-                    options[^1] = "Go Back";
-                    counter++;
-                }
-                else
-                {
-                    options[counter] = DateTimeFormatInfo.CurrentInfo.GetMonthName(i);
-                    counter++;
-                }
-
-
-            }
-
+            options[counter] = DateTimeFormatInfo.CurrentInfo.GetMonthName(cur_month);
+            options[^1] = "Go Back";
             string prompt = $"make a reservation for one these months: \n{DateTime.UtcNow.Date.Year}";
             while (true)
             {
-            int input = _myMenu.RunMenu(options, prompt);
-            //Ask to po how far reservations should be
-            // ask times for reservations
-            switch (input)
-            {
-                case 0:
-                    SwitchMonthMethod(options,options[input]);
-                    break;
-                case 1:
-                    SwitchMonthMethod(options,options[input]);
-                    break;
-                
-                case 2:
-                    SwitchMonthMethod(options,options[input]);
-                    break;
-                    
-                case 3:
-                    SwitchMonthMethod(options,options[input]);
-                    break;
-                    
-                case 4:
-                    SwitchMonthMethod(options,options[input]);
-                    break;
-                case 5:
-                    SwitchMonthMethod(options,options[input]);
-                    break;
-                case 6:
-                    SwitchMonthMethod(options,options[input]);
-                    break;
-                case 7:
-                    SwitchMonthMethod(options,options[input]);
-                    break;
-                case 8:
-                    SwitchMonthMethod(options,options[input]);
-                    break;
-                case 9:
-                    SwitchMonthMethod(options,options[input]);
-                    break;
-                case 10:
-                    SwitchMonthMethod(options,options[input]);
-                    break;
-                case 11:
-                    SwitchMonthMethod(options,options[input]);
-                    break;
-                case 12:
-                    SwitchMonthMethod(options,options[input]);
-                    break;
-                
-                default:
-                    break;
-
+                int input = _myMenu.RunMenu(options, prompt);
+                switch (input)
+                {
+                    case 0:
+                        SwitchMonthMethod(options, options[input]);
+                        break;
+                    case 1:
+                        SwitchMonthMethod(options, options[input]);
+                        break;
+                }
             }
-        }
     }
 }
