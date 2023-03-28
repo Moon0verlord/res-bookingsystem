4f4bc1c44e6cfdb5c33928b018f1a2c7a7a1c2f0
WarMartijn <1058740@hr.nl>
2023-03-28 15:08:57 +0200

    Added a system for tables

---
+++ Project/Logic/TableLogic.cs
@@ -0,0 +1,106 @@
+class TableLogic
+{
+    public IDictionary<int, Dictionary<int, bool>> isTableFree = new Dictionary<int, Dictionary<int, bool>>();
+
+    public void occupyTable(int tableNumber)
+    {
+        isTableFree[1][tableNumber] = false;
+    }
+
+    public void freeTable(int tableNumber)
+    {
+        isTableFree[1][tableNumber] = true;
+    }
+
+    public TableLogic(){
+        for (int i = 1; i < 9; i++)
+        {
+            isTableFree.Add(i, new Dictionary<int, bool>());
+            isTableFree[i].Add(2, true);
+        }
+        for (int j = 9; j < 14; j++)
+        {
+            isTableFree.Add(j, new Dictionary<int, bool>());
+            isTableFree[j].Add(4, true);
+        }
+        for (int k = 14; k < 16; k++)
+        {
+            isTableFree.Add(k, new Dictionary<int, bool>());
+            isTableFree[k].Add(6, true);
+        }
+        ShowTable();
+    }
+
+    public void ShowTable()
+    {
+        bool firstTable2 = true;
+        bool firstTable4 = true;
+        bool firstTable6 = true;
+        Console.Clear();
+        Console.WriteLine("Table\tStatus");
+        for (int i = 1; i <= isTableFree.Count; i++)
+        foreach (var table in isTableFree[i])
+        {
+            if (table.Key == 2)
+            {
+                if (firstTable2)
+                {
+                    Console.WriteLine("2 person tables");
+                    firstTable2 = false;
+                }
+                if (table.Value)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                Console.WriteLine(i + "\t" + (table.Value ? "Free" : "Occupied"));
+                Console.ResetColor();
+            }
+            else if (table.Key == 4)
+            {
+                if (firstTable4)
+                {
+                    Console.WriteLine("4 person tables");
+                    firstTable4 = false;
+                }
+                if (table.Value)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                Console.WriteLine(i + "\t" + (table.Value ? "Free" : "Occupied"));
+                Console.ResetColor();
+            }
+            else
+            {
+                if (firstTable6)
+                {
+                    Console.WriteLine("6 person tables");
+                    firstTable6 = false;
+                }
+                if (table.Value)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                Console.WriteLine(i + "\t" + (table.Value ? "Free" : "Occupied"));
+                Console.ResetColor();
+            }
+        }
+    }
+
+    public TableLogic Start()
+    {
+        TableLogic table = new TableLogic();
+        return table;
+    }
+}
