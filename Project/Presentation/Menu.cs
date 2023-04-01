b1f5a567fff0e71531d0097534a5361f66ace6ff
Moon0verlord <114489960+Moon0verlord@users.noreply.github.com>
2023-04-01 20:38:33 +0200

    Worked on making ManageMenu function work using linq fixed json added todo comments

--- Project/Presentation/Menu.cs
+++ Project/Presentation/Menu.cs
@@ -16,16 +16,16 @@ public static class Dishes
         switch (input)
         {
             case 0:
-                JsonCursor("vegetarian");
+                JsonCursor("Vegetarisch");
                 break;
             case 1:
-                JsonCursor("fish");
+                JsonCursor("Vis");
                 break;
             case 2:
-                JsonCursor("meat");
+                JsonCursor("Vlees");
                 break;
             case 3:
-                JsonCursor("vegan");
+                JsonCursor("Veganistisch");
                 break;
             case 4:
                 MainMenu.Start();
@@ -40,26 +40,26 @@ public static class Dishes
     {
         string path = System.IO.Path.GetFullPath(System.IO.Path.Combine(Environment.CurrentDirectory, @"DataSources/Menu.json"));
         string json = File.ReadAllText(path);
-        JObject menu = JObject.Parse(json);
-        Console.WriteLine("Dishes:");
+        JObject dishes = JObject.Parse(json);
+        Console.WriteLine("Gerechten:");
         Console.WriteLine("-------");
 
         Console.WriteLine("2 Courses:");
-        foreach (var course in menu[choice]["2_courses"])
+        foreach (var course in dishes[choice]["2_courses"])
         {
-            string appetizer = (string)course["appetizer"];
-            string entree = (string)course["entree"];
+            string appetizer = (string)course["Voorgerecht"];
+            string entree = (string)course["Maaltijd"];
             Console.WriteLine($"Appetizer: {appetizer}");
             Console.WriteLine($"Entree: {entree}");
             Console.WriteLine();
         }
 
         Console.WriteLine("3 Courses:");
-        foreach (var course in menu[choice]["3_courses"])
+        foreach (var course in dishes[choice]["3_courses"])
         {
-            string appetizer = (string)course["appetizer"];
-            string entree = (string)course["entree"];
-            string dessert = (string)course["dessert"];
+            string appetizer = (string)course["Voorgerecht"];
+            string entree = (string)course["Maaltijd"];
+            string dessert = (string)course["Nagerecht"];
             Console.WriteLine($"Appetizer: {appetizer}");
             Console.WriteLine($"Entree: {entree}");
             Console.WriteLine($"Dessert: {dessert}");
@@ -67,12 +67,12 @@ public static class Dishes
         }
 
         Console.WriteLine("4 Courses:");
-        foreach (var course in menu[choice]["4_courses"])
+        foreach (var course in dishes[choice]["4_courses"])
         {
-            string appetizer = (string)course["appetizer"];
-            string soup = (string)course["soup"];
-            string entree = (string)course["entree"];
-            string dessert = (string)course["dessert"];
+            string appetizer = (string)course["Voorgerecht"];
+            string soup = (string)course["Soep"];
+            string entree = (string)course["Maaltijd"];
+            string dessert = (string)course["Nagerecht"];
             Console.WriteLine($"Appetizer: {appetizer}");
             Console.WriteLine($"Soup: {soup}");
             Console.WriteLine($"Entree: {entree}");
@@ -84,28 +84,51 @@ public static class Dishes
         }
     }
     
-    // W.I.P - skeleton for managing dishes in the menu
+    // W.I.P - working skeleton for managing dishes in the menu
     public static void ManageMenu()
     {
+       
+        Console.WriteLine("Which type of dish would you like to add?");
+        Console.WriteLine("1. Vegetarian");
+        Console.WriteLine("2. Fish");
+        Console.WriteLine("3. Meat");
+        Console.WriteLine("4. Vegan");
+        Console.WriteLine("5. Back to main menu");
+        //console.readline - string choice1 = "Vegetarisch";
+        Console.WriteLine("Which course would you like to add?");
+        Console.WriteLine("1. 2 Courses");
+        Console.WriteLine("2. 3 Courses");
+        Console.WriteLine("3. 4 Courses");
+        Console.WriteLine("4. Back to main menu");
+        //console.readline - string choice2 = "4_Gangen";
+        
+        // TODO better naming for json variables
+        //TODO Turn skeleton into a fully working method
         string path = System.IO.Path.GetFullPath(System.IO.Path.Combine(Environment.CurrentDirectory, @"DataSources/accounts.json"));
         string json = File.ReadAllText("Dishes.json");
-        JObject menu = JObject.Parse(json);
-        Console.WriteLine("Which type of dish would you like to add?");
-        JObject Vegetarian = (JObject)menu["vegetarian"];
-        JArray vm = (JArray)Vegetarian["2_courses"];
-        // prints out the vegetarian appetizers
-        foreach (var item in vm)
-        {
-            Console.WriteLine(item["appetizer"]);
-        }
-        vm.Add(new JObject(new JProperty("appetizer", "test"), new JProperty("entree", "test")));
-        //Console.WriteLine(menu.ToString());
+        JObject dishes = JObject.Parse(json);
+
+        string menu = File.ReadAllText("Menu.json");
+        JObject menuobj = JObject.Parse(menu);
+
+        string choice1 = "Vegetarisch";
+        string choice2 = "4_Gangen";
+
+        JObject dish = (JObject)dishes[choice1];
+        JArray vm = (JArray)dish[choice2];
+        JObject itemA = (JObject)vm.ElementAt(0);
+
+        JArray vm2 = (JArray)menuobj[choice1][choice2];
+        vm2.Add(itemA);
+
+        File.WriteAllText("Menu.json", menuobj.ToString());
+
         // move json back to DataSources
         // ask what type of dish
         // ask what course
         // add course to json
         
-        File.WriteAllText("Menu.json", Vegetarian.ToString());
+        //File.WriteAllText("Menu.json", vm[0].ToString());
         
     }
 
