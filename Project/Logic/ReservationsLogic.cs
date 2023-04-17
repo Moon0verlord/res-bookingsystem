ab5d800cb4bca495bfecb3fc4a78e6a3dca694f8
Rokimajo <robinvliegtvandaag@gmail.com>
2023-04-17 15:22:02 +0200

    Merged new ReservationsLogic into main branch

--- Project/Logic/ReservationsLogic.cs
+++ Project/Logic/ReservationsLogic.cs
@@ -7,81 +7,106 @@ class ReservationsLogic
     // empty constructor to call CurMonth
     public ReservationsLogic() {}
 
-    public List<DateTime> PopulateDates()
+    public DateTime[,] PopulateDates()
     {
-        var thisWeek = new List<DateTime>();
-        var DaysInMonth = DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month);
-        for (int i = DateTime.Now.Day; i <= DaysInMonth; i++)
+        int rowCount = 0;
+        int columnCount = 0;
+        // Kijk hoeveel rijen de array nodig heeft door de maand op te delen in weken
+        int amountOfRows = (int)DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month) / 7;
+        DateTime[,] dates = new DateTime[amountOfRows,7];
+        DateTime currentDate = DateTime.Today;
+        do
         {
-            if(i!=DaysInMonth){
-            thisWeek.Add(DateTime.Today.AddDays(i));
-        }
-            else if (i == DaysInMonth)
+            if (currentDate.Date != DateTime.MinValue) dates[rowCount, columnCount] = currentDate.Date;
+            columnCount++;
+            if (columnCount % 7 == 0)
             {
-                thisWeek.Add(new DateTime(DateTime.Now.Year, DateTime.Now.Month,
-                    DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month)));
+                rowCount++;
+                columnCount = 0;
             }
-        }
-        return thisWeek;
-    }
+            currentDate = currentDate.AddDays(1);
+        } while (currentDate.Month == DateTime.Today.Month);
 
-    public List<TimeSpan> PopulateTimes()
-    {
-        var timeList = new List<TimeSpan>();
-        for (int hours = 16; hours < 22; hours++)
-        {
-            for(int minutes = 0;minutes<=30;minutes+=30)
-            {
-                timeList.Add(new TimeSpan(hours, minutes, 0));
-                if(hours == 21 && minutes == 30)
-                {
-                    break;
-                }
-            }
-        }
-        
-        return timeList;
+        return dates;
     }
-
-    public List<ReservationModel> PopulateTables(DateTime res_Date)
+    
+    public List<ReservationModel> PopulateTables(DateTime res_Date, (TimeSpan, TimeSpan) chosenTime)
     {
         List<ReservationModel> reservedTables = AccountsAccess.LoadAllReservations();
         List<ReservationModel> tablesToAdd = new List<ReservationModel>();
-
+        // these are needed to set each table's maximum allowed size of 2, 4 and 6.
+        // Starts at -1 so that the first if statement makes the index start at 0.
+        List<int> CurrentTableSizes = new List<int>() { 2, 4, 6 };
+        int tableIndex = -1;
+        
         for (int i = 1; i <= 15; i++)
         {
-            IEnumerable<ReservationModel> tablesWithThisID = reservedTables.Where(res => res.Id.Equals(i));
-            if (i == 1 || i == 9 || i == 14) tablesToAdd.Add(null);
+            IEnumerable<ReservationModel> tablesWithThisID = reservedTables.Where(res => res.Id.Equals(i)&&res.Date==res_Date);
+            if (i == 1 || i == 9 || i == 14)
+            {
+                tablesToAdd.Add(null);
+                tableIndex++;
+            }
             if (tablesWithThisID.Count() >= 1)
             {
                 foreach (ReservationModel table in tablesWithThisID)
                 {
-                    if (table.Date == res_Date)
+                    if (table.Date == res_Date.Date)
                     {
-                        tablesToAdd.Add(table);
+                        if (table.StartTime >= chosenTime.Item1 && table.LeaveTime <= chosenTime.Item2)
+                        {
+                            table.isReserved = true;
+                            table.TableSize = CurrentTableSizes[tableIndex];
+                            tablesToAdd.Add(table);
+                        }
+                        else
+                        {
+                            bool noDuplicates = true;
+                            foreach (ReservationModel check in tablesToAdd)
+                            {
+                                if (check != null)
+                                {
+                                    if (check.Id == i)
+                                        noDuplicates = false;
+                                }
+                            }
+                            if (noDuplicates)
+                            {
+                                int size = CurrentTableSizes[tableIndex];
+                                var AddTable = AddDefaultTable(i, size);
+                                tablesToAdd.Add(AddTable);
+                                
+                            }
+                        }
                     }
-                    else
+                    else 
                     {
-                        ReservationModel resm = new ReservationModel(i, null!, new DateTime(0));
-                        resm.isReserved = false;
-                        tablesToAdd.Add(resm);
+                        int size = CurrentTableSizes[tableIndex];
+                        tablesToAdd.Add(AddDefaultTable(i, size));
                     }
                 }
             }
             else
             {
-                ReservationModel resm = new ReservationModel(i, null!, new DateTime(0));
-                resm.isReserved = false;
-                tablesToAdd.Add(resm);
+                int size = CurrentTableSizes[tableIndex];
+                tablesToAdd.Add(AddDefaultTable(i, size));
             }
         }
         return tablesToAdd;
     }
 
-
-    public void CreateReservation(string email, DateTime res_Date, int chosenTable)
+    public ReservationModel AddDefaultTable(int id, int size)
     {
-        ReservationModel newReservation = new ReservationModel(chosenTable, email, res_Date);
+        ReservationModel resm = new ReservationModel(id, null, new DateTime(0), 0, default, default);
+        resm.isReserved = false;
+        resm.TableSize = size;
+        return resm;
+    }
+
+
+    public void CreateReservation(string email, DateTime res_Date, int chosenTable, int groupsize, TimeSpan entertime, TimeSpan leavetime)
+    {
+        ReservationModel newReservation = new ReservationModel(chosenTable, email, res_Date, groupsize, entertime, leavetime);
         AccountsAccess.AddReservation(newReservation);
     }
 }
