8cb0527515258eef37e4968c7ad72870c725dcf3
Rokimajo <robinvliegtvandaag@gmail.com>
2023-04-17 15:23:03 +0200

    Merged branch Reservations into main branch

--- Project/Presentation/Reservation.cs
+++ Project/Presentation/Reservation.cs
@@ -1,18 +1,24 @@
-using System.ComponentModel.Design;
+ using System.ComponentModel.Design;
 using System.ComponentModel.Design.Serialization;
 using System.Globalization;
-using System;
-using System.Collections;
-using System.Collections.Specialized;
-
 static class Reservation
 {
-    private static MenuLogic _myMenu = new MenuLogic();
+    private static MenuLogic _my1DMenu = new MenuLogic();
+    private static _2DMenuLogic _my2DMenu = new _2DMenuLogic();
     private static readonly ReservationsLogic Reservations = new ReservationsLogic();
-    
-    // starts the reservation process
+    private static string userEmail;
+    private static int amountOfPeople;
+    private static DateTime chosenDate;
+    private static (TimeSpan, TimeSpan) chosenTimeslot;
+    private static int chosenTable;
+
     public static void ResStart(AccountModel acc = null)
     {
+        userEmail = null;
+        amountOfPeople = 0;
+        chosenDate = default;
+        chosenTimeslot = (default, default);
+        chosenTable = 0;
         Console.Clear();
         if (acc == null)
         {
@@ -22,8 +28,8 @@ static class Reservation
             {
                 Console.Clear();
                 string prompt = "Vul hier uw e-mail in om een reservatie te maken.";
-                string[] options = { $"Vul hier uw e-mail in" + (email == null ? "\n" : $": {email}\n"), "Doorgaan","Reservering bekijken", "Afsluiten" };
-                int selectedIndex = _myMenu.RunMenu(options, prompt);
+                string[] options = { $"Vul hier uw e-mail in" + (email == null ? "\n" : $": {email}\n"), "Doorgaan", "Afsluiten" };
+                int selectedIndex = _my1DMenu.RunMenu(options, prompt);
                 switch (selectedIndex)
                 {
                     case 0:
@@ -44,7 +50,8 @@ static class Reservation
                         if (email != null)
                         {
                             loop = false;
-                            ResMenu(email);
+                            userEmail = email;
+                            ResMenu();
                         }
                         else
                         {
@@ -55,49 +62,38 @@ static class Reservation
                         }
                         break;
                     case 2:
-                        ViewRes(email);
-                        break;
-                    case 3:
                         MainMenu.Start();
-                            break;
+                        break;
                 }
             }
         }
         else
         {
-            ResMenu(acc.EmailAddress);
+            userEmail = acc.EmailAddress;
+            ResMenu();
         }
         //show available hours on requested date
         //User can select an hour
         //back to menu
     }
 
-    public static void ResMenu(string email)
+    public static void ResMenu()
     {
         Console.Clear();
-        
-        Console.WriteLine($"U kunt alleen een reservatie maken voor de hudige maand ({ReservationsLogic.CurMonth})\nKies een dag van de week: \n");
-        var thisWeek = Reservations.PopulateDates();
-        foreach (DateTime date in thisWeek)
-        {
-            Console.Write($"{date.ToString("ddd", CultureInfo.InvariantCulture)}\t");
-        }
-        var dictChoice = _myMenu.RunMenu(thisWeek, "", false);
-        DateTime res_Date = ChooseTime(dictChoice);
-        int chosenTable = ChooseTable(res_Date);
+        amountOfPeople = ChooseGroupSize();
+        if (amountOfPeople <= 0) ResStart();
+        else ChooseDate();
         Console.ForegroundColor = ConsoleColor.Green;
         Console.Clear();
-        Console.WriteLine($"Email:{email}\nReservatie tafel nummer: {chosenTable}\nDatum: {res_Date.Date.ToString("dd-MM-yyyy")}" +
-                          $"\nTijd: {res_Date.TimeOfDay.ToString("hh\\:mm")}\nWeet u zeker dat u deze tijd wil reserveren? (j/n): ");
+        //chosenTimeslot item 1 is time when entering, item 2 is time when leaving.
+        Console.WriteLine($"Email:{userEmail}\nReservatie tafel nummer: {chosenTable}\nDatum: {chosenDate.Date.ToString("dd-MM-yyyy")}" +
+                          $"\nTijd: ({chosenTimeslot.Item1} - {chosenTimeslot.Item2})\nWeet u zeker dat u deze tijd wil reserveren? (j/n): ");
         Console.ResetColor();
         string answer = Console.ReadLine()!;
         switch (answer.ToLower())
         {
-            case "ja":
-            case "j":
-            case "Ja":
-                Reservations.CreateReservation(email, res_Date, chosenTable);
-                EmailLogic.SendEmailNoAccount(email, "Naam", chosenTable, res_Date);
+            case "ja": case "j":
+                Reservations.CreateReservation(userEmail, chosenDate, chosenTable, amountOfPeople, chosenTimeslot.Item1, chosenTimeslot.Item2);
                 Console.Clear();
                 Console.WriteLine("\nReservatie is gemaakt.");
                 Thread.Sleep(1500);
@@ -105,88 +101,177 @@ static class Reservation
         }
     }
 
-    public static DateTime ChooseTime(Dictionary<int, DateTime> dictChoice)
+    public static int ChooseGroupSize()
     {
-        Console.Clear();
-        string prompt = "Kies hier een tijd voor de geselcteerde datum " +
-                          $"({dictChoice.Select(i => i.Value).FirstOrDefault().ToString("dd-MM-yyyy")})";
-        var timeList = Reservations.PopulateTimes();
-        int selectIndex = _myMenu.RunMenu(timeList.Select(i => i.ToString("hh\\:mm")).ToArray(), prompt, sideways: true, displayTime: true);
-        DateTime res_Date = dictChoice.Select(i => i.Value).FirstOrDefault().Date + timeList[selectIndex];
-        return res_Date;
-    }
-
-    public static int ChooseTable(DateTime res_Date)
-    {
-        var tablesOnly = Reservations.PopulateTables(res_Date);
-        int selectedTable = _myMenu.RunTableMenu(tablesOnly, "", false);
-        return selectedTable;
-    }
-
-    public static void ViewRes(string Email)
-    {
-        Console.Clear();
-        bool CheckIfRes = false;
-        List<ReservationModel> AllRes = AccountsAccess.LoadAllReservations();
-        List<string> ReservationsPerson = new();
-        List<int> ReservationPersonPositions = new();
-        //$"U heeft een reservering onder de Email: {res.EmailAddress}. Voor tafel {res.Id} en De datum van de resevering is: {res.Date}."
-        foreach (ReservationModel res in AllRes)
+        string amountofPeople = default;
+        while (true)
         {
-            if (Email == res.EmailAddress && res.Date > DateTime.Now)
+            Console.ResetColor();
+            string[] options = new[]
+                { "Vul hier in met hoeveel mensen u komt.", "Ga terug" };
+            int selectedIndex = _my1DMenu.RunMenu(options, "Kies hier uw groepsgrootte:");
+            Console.Clear();
+            switch (selectedIndex)
             {
-                ReservationsPerson.Add($"U heeft een reservering onder de Email: {res.EmailAddress}. Voor tafel {res.Id} en De datum van de resevering is: {res.Date}.");
-                ReservationPersonPositions.Add(AllRes.FindIndex(a => a == res));
-                CheckIfRes = true;
+                case 0:
+                    Console.Write("Typ hier met hoeveel mensen u van plan bent te komen: ");
+                    amountofPeople = Console.ReadLine()!;
+                    bool success = int.TryParse(amountofPeople, out int number);
+                    if (!success)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Voer enkel geldige nummers in, alstublieft.");
+                        Thread.Sleep(2500);
+                        amountofPeople = default;
+                        break;
+                    }
+                    else
+                    {
+                        if (number <= 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine(
+                                "Nummers kleiner of gelijk aan nul zijn niet toegestaan. Voer opnieuw in.");
+                            Thread.Sleep(2500);
+                            amountofPeople = default;
+                            break;
+                        }
+                        else if (number > 6)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine(
+                                "U moet bellen voor groepsgroottes boven de zes personen, bekijk onze contactinformatie in het hoofdmenu.");
+                            Thread.Sleep(2500);
+                            amountofPeople = default;
+                            break;
+                        }
+                        else return number;
+                    }
+                    break;
+                case 1:
+                    return 0;
+                    break;
             }
         }
-
-        if (CheckIfRes == false)
+    }
+    public static void ChooseDate()
+    {
+        Console.Clear();
+        var thisMonth = Reservations.PopulateDates();
+        Console.WriteLine($"U kunt alleen een reservatie maken voor de huidige maand ({ReservationsLogic.CurMonth})\nKies een datum (of druk op 'q' om terug te gaan):\n");
+        for (int i = 0; i < thisMonth.GetLength(1); i++)
         {
-            Console.WriteLine("Er staan nog geen reserveringen open met dit emailaddres.");
+            Console.Write($"{thisMonth[0, i].Date.ToString("ddd", CultureInfo.GetCultureInfo("nl"))}\t");
         }
+        DateTime userChoice = _my2DMenu.RunMenu(thisMonth, "", false);
+        if (userChoice == default) ResMenu();
         else
         {
-            ReservationsPerson.Add("Ga terug");
-            while (true)
+            chosenDate = userChoice;
+            ChooseTimeslot();
+        }
+    }
+    public static void ChooseTimeslot()
+    {
+        TimeSpan ts1 = default;
+        TimeSpan ts2 = default;
+        string[] options = new[] { "16:00 - 18:00", "18:00 - 20:00", "20:00 - 22:00", "Ga terug"};
+        int selectedIndex = _my1DMenu.RunMenu(options, "Kies uw gewenste tijdslot:");
+        switch (selectedIndex)
+        {
+            case 0:
+                ts1 = new TimeSpan(0, 16, 0, 0);
+                ts2 = new TimeSpan(0, 18, 0, 0);
+                chosenTimeslot = (ts1, ts2);
+                ChooseTable(chosenDate, chosenTimeslot);
+                break;
+            case 1:
+                ts1 = new TimeSpan(0, 18, 0, 0);
+                ts2 = new TimeSpan(0, 20, 0, 0);
+                chosenTimeslot = (ts1, ts2);
+                ChooseTable(chosenDate, chosenTimeslot);
+                break;
+            case 2:
+                ts1 = new TimeSpan(0, 20, 0, 0);
+                ts2 = new TimeSpan(0, 22, 0, 0);
+                chosenTimeslot = (ts1, ts2);
+                ChooseTable(chosenDate, chosenTimeslot);
+                break;
+            case 3:
+                ChooseDate();
+                break;
+        }
+    }
+    public static void ChooseTable(DateTime res_Date, (TimeSpan, TimeSpan) chosenTime)
+    {
+        var tablesOnly = Reservations.PopulateTables(res_Date, chosenTime);
+        int selectedTable = _my1DMenu.RunTableMenu(tablesOnly, "Kies uw tafel (of druk op 'q' om terug te gaan):", amountOfPeople);
+        if (selectedTable != 0) chosenTable = selectedTable;
+        else ChooseTimeslot();
+    }
+    
+    public static void ViewRes(string Email)
+{
+    Console.Clear();
+    bool CheckIfRes = false;
+    List<ReservationModel> AllRes = AccountsAccess.LoadAllReservations();
+    List<string> ReservationsPerson = new();
+    List<int> ReservationPersonPositions = new();
+    //$"U heeft een reservering onder de Email: {res.EmailAddress}. Voor tafel {res.Id} en De datum van de resevering is: {res.Date}."
+    foreach (ReservationModel res in AllRes)
+    {
+        if (Email == res.EmailAddress && res.Date > DateTime.Now)
+        {
+            ReservationsPerson.Add($"U heeft een reservering onder de Email: {res.EmailAddress}. Voor tafel {res.Id} en De datum van de resevering is: {res.Date}.");
+            ReservationPersonPositions.Add(AllRes.FindIndex(a => a == res));
+            CheckIfRes = true;
+        }
+    }
+
+    if (CheckIfRes == false)
+    {
+        Console.WriteLine("Er staan nog geen reserveringen open met dit emailaddres.");
+    }
+    else
+    {
+        ReservationsPerson.Add("Ga terug");
+        while (true)
+        {
+            var reserv_input = _my1DMenu.RunMenu(ReservationsPerson.ToArray(), "Reserveringen");
+            switch (ReservationsPerson[reserv_input])
             {
-                var reserv_input = _myMenu.RunMenu(ReservationsPerson.ToArray(), "Reserveringen");
-                switch (ReservationsPerson[reserv_input])
-                {
-                    case "Ga terug":
-                        MainMenu.Start();
-                        break;
-                    default:
-                        Console.Clear();
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Wilt u uw reservering annuleren? (j/n)");
-                        Console.ResetColor();
-                        var Choice = Console.ReadLine();
-                        switch (Choice)
-                        {
-                            case "J":
-                            case "j":
-                            case "Ja":
-                                case "ja":
-                                Console.WriteLine("De reservatie is verwijderd");
-                                AllRes.RemoveAt(ReservationPersonPositions[reserv_input]);
-                                ReservationsPerson.RemoveAt(reserv_input);
-                                AccountsAccess.WriteAllReservations(AllRes);
-                                Thread.Sleep(5000);
-                                break;
-                            case "N":case "n":case "Nee":case "nee":
-                                Console.WriteLine("De reservatie is niet verwijderd");
-                                Thread.Sleep(2000);
-                                break;
-                                
-                        }
-
-                        break;
-
-
-                }
-
+                case "Ga terug":
+                    MainMenu.Start();
+                    break;
+                default:
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Wilt u uw reservering annuleren? (j/n)");
+                    Console.ResetColor();
+                    var Choice = Console.ReadLine();
+                    switch (Choice)
+                    {
+                        case "J":
+                        case "j":
+                        case "Ja":
+                            case "ja":
+                            Console.WriteLine("De reservatie is verwijderd");
+                            AllRes.RemoveAt(ReservationPersonPositions[reserv_input]);
+                            ReservationsPerson.RemoveAt(reserv_input);
+                            AccountsAccess.WriteAllReservations(AllRes);
+                            Thread.Sleep(5000);
+                            break;
+                        case "N":case "n":case "Nee":case "nee":
+                            Console.WriteLine("De reservatie is niet verwijderd");
+                            Thread.Sleep(2000);
+                            break;
+                            
+                    }
+                    break;
             }
         }
     }
+}
+
+
 }
