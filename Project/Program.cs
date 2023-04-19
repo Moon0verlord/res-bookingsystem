e193bebc12fc0ff2e217e017457d5652102fa278
Rokimajo <robinvliegtvandaag@gmail.com>
2023-04-19 16:30:34 +0200

    Added SetWindowSize() so that users don't accidentally create bounding errors.

--- Project/Program.cs
+++ Project/Program.cs
@@ -2,6 +2,8 @@
 {
     private static void Main()
     {
+        // set windowsize here to make sure there's no bounding errors later in the program.
+        Console.SetWindowSize(180, 35);
         Console.Clear();
         MainMenu.Start();
     }
