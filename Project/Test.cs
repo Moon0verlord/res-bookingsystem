c3c32c78377a9bb5faa25d9d61a16b875d4e31b4
WarMartijn <1058740@hr.nl>
2023-04-27 17:16:23 +0200

    Added a unit test

---
+++ Project/Test.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Testing;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        Assert.IsTrue(1==1);
+    }
+}
