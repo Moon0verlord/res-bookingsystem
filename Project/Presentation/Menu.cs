62d069768af001741b37a52c1cff93943269207c
Moon0verlord <114489960+Moon0verlord@users.noreply.github.com>
2023-05-23 19:56:34 +0200

    Removed useless usings

--- Project/Presentation/Menu.cs
+++ Project/Presentation/Menu.cs
@@ -1,6 +1,4 @@
-﻿using System.Globalization;
-using System.Text;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 using Newtonsoft.Json.Linq;
 
 namespace Project.Presentation;
