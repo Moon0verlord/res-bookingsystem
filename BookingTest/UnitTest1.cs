9bfba36504f9ccca114bffe0a0c596b97ad5af7b
Robin Otterspeer <robin.otterspeer2005@gmail.com>
2023-05-29 16:48:19 +0100

    add unit test for answer

--- BookingTest/UnitTest1.cs
+++ BookingTest/UnitTest1.cs
@@ -18,19 +18,19 @@ public class UnitTest1
     public void AccountTest()
     {
         AccountModel model = new AccountModel(1, "@.@", "xyz", "Mark", false, false);
-        Assert.IsTrue(model.Id==1);
+        Assert.IsTrue(model.Id == 1);
     }
-    
+
     [TestMethod]
-    [DataRow(1, "Test1", "8D0123", "Test", false,false)]
-    [DataRow(2, "Test2", "328W90", "Test1", true,false)]
-    [DataRow(3, "Test3", "423P223", "Bigtest", false,false)]
-    [DataRow(4, "Test4", "31A21321", "Test4", true,false)]
+    [DataRow(1, "Test1", "8D0123", "Test", false, false)]
+    [DataRow(2, "Test2", "328W90", "Test1", true, false)]
+    [DataRow(3, "Test3", "423P223", "Bigtest", false, false)]
+    [DataRow(4, "Test4", "31A21321", "Test4", true, false)]
     public void TestAccount(int id, string email, string password, string name, bool employee, bool manager)
     {
-       
-        AccountModel account = new AccountModel(id, email, password, name, employee,manager);
-        AccountsAccess.AddAccount(account.EmailAddress,account.Password,account.FullName,account.IsEmployee,account.IsManager);
+
+        AccountModel account = new AccountModel(id, email, password, name, employee, manager);
+        AccountsAccess.AddAccount(account.EmailAddress, account.Password, account.FullName, account.IsEmployee, account.IsManager);
         Assert.IsNotNull(AccountsAccess.LoadAll().Find(account => account.EmailAddress == email));
         AccountsAccess.ClearJsonFiles(1);
     }
@@ -40,17 +40,17 @@ public class UnitTest1
     {
         //Tried to add a datarow to this method, the datetime and timespan functions woudltn cooperate so i had to do this
         ReservationsLogic reservations = new ReservationsLogic();
-        AccountsAccess.AddReservation(new ReservationModel("1S","Test@gmail.com",new DateTime(2003,12,2),4,new TimeSpan(12),new TimeSpan(13),"RES-180439", 2));
+        AccountsAccess.AddReservation(new ReservationModel("1S", "Test@gmail.com", new DateTime(2003, 12, 2), 4, new TimeSpan(12), new TimeSpan(13), "RES-180439", 2));
         var Reservation = AccountsAccess.LoadAllReservations().Find(account => account.EmailAddress == "Test@gmail.com");
-        Assert.IsTrue(Reservation != null );
+        Assert.IsTrue(Reservation != null);
         AccountsAccess.ClearJsonFiles(2);
-        
+
     }
-    
-    
+
+
     // made by robin b
     // create id used by reservations, and check if it matches the following regular expression.
-    [TestMethod] 
+    [TestMethod]
     public void TestIDCreation()
     {
         ReservationsLogic logic = new ReservationsLogic();
@@ -58,11 +58,11 @@ public class UnitTest1
         string id = logic.CreateID();
         Assert.IsTrue(regex.IsMatch(id));
     }
-    
+
     // made by robin b
     // test if you get dates back when calling populate dates()
     // and that the correct result is a 2D datetime array
-    [TestMethod] 
+    [TestMethod]
     public void TestPopulateDates()
     {
         ReservationsLogic logic = new ReservationsLogic();
@@ -70,7 +70,7 @@ public class UnitTest1
         Assert.IsNotNull(result);
         Assert.IsTrue(result.GetType() == typeof(DateTime[,]));
     }
-    
+
     // made by Jona
     // tests if the email is valid
     [TestMethod]
@@ -89,10 +89,10 @@ public class UnitTest1
         email = "test@gmail.com";
         Assert.IsTrue(EmailLogic.IsValidEmail(email));
     }
-    
-    
+
+
     // Made by Rafiq
-    
+
     [TestMethod]
     public void HidePass_ReturnsHiddenPassword()
     {
@@ -102,4 +102,15 @@ public class UnitTest1
         Assert.AreEqual(expectedHiddenPassword, actualHiddenPassword);
     }
 
+    // Made by Robin O
+    // test if the input is correct
+
+    [TestMethod]
+    public void TestCheckInput()
+    {
+        string answer = "j";
+        int expectedanswer = 1;
+        int actualanswer = AnswerLogic.CheckInput(answer);
+        Assert.AreEqual(expectedanswer, actualanswer);
+    }
 }
