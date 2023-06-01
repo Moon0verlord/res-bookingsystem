dc26e6ee960e7d6adff8916dfd96ed76982eef5a
WarMartijn <1058740@hr.nl>
2023-06-01 15:57:14 +0200

    Improved Main

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,15 +1,18 @@
-﻿using System;
+﻿
 using System.Runtime.InteropServices;
 public class Program
 {
     private static void Main()
     {
-        // set windowsize here to make sure there's no bounding errors later in the program.
-        try
+        // set windows window size here to make sure there's no bounding errors later in the program.
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
         {
             Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
         }
-        catch { }
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            Console.WriteLine("Ha ha apple");
+        }
         Console.Clear();
         MainMenu.Start();
     }
