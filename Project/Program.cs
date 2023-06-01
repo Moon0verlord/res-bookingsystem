af0c02eff2426aa7acdc9060acab460120f0ed28
WarMartijn <1058740@hr.nl>
2023-06-01 16:34:30 +0200

    comment

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,10 +1,9 @@
-﻿
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 public class Program
 {
     private static void Main()
     {
-        // set windows window size here to make sure there's no bounding errors later in the program.
+        // set window size here to make sure there's no bounding errors later in the program.
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
         {
             Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
