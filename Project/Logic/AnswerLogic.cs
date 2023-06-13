1d8f1c76e534f212871589bb18234add7f3ab4d7
WarMartijn <1058740@hr.nl>
2023-06-13 10:42:12 +0200

    Changed if to switch

--- Project/Logic/AnswerLogic.cs
+++ Project/Logic/AnswerLogic.cs
@@ -1,33 +1,35 @@
 using System.Text.RegularExpressions;
 public class AnswerLogic
 {
-
     public static int CheckInput(string answer)
     {
         answer = answer.ToLower();
-        //1 is correct output
-        //0 is false output
-        //-1 is error
+
         string pattern = @"^j[a]+$";
         string pattern2 = @"^n[e]+$";
-       
-        
-            if (Regex.IsMatch(answer.ToLower(),pattern)||answer =="j")
-            {
+
+        switch (answer)
+        {
+            case "j":
                 return 1;
-            } 
-            if (Regex.IsMatch(answer.ToLower(),pattern2)||answer =="n")
-            {
+            case "n":
                 return 0;
-            }
-            Console.Clear();
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Incorrecte Invoer");
-            Thread.Sleep(2000);
-            UserLogin.DiscardKeys();
-            Console.ResetColor();
-            return -1;
-            
-            
+            default:
+                if (Regex.IsMatch(answer, pattern))
+                {
+                    return 1;
+                }
+                if (Regex.IsMatch(answer, pattern2))
+                {
+                    return 0;
+                }
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Incorrecte Invoer");
+                Thread.Sleep(2000);
+                UserLogin.DiscardKeys();
+                Console.ResetColor();
+                return -1;
+        }
     }
 }
