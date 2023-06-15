c2191f3585f2eb78970c1e377a201d488990c9dd
WarMartijn <1058740@hr.nl>
2023-06-15 13:35:50 +0200

    Adjusted Program message dimensions

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,14 +1,20 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 public class Program
 {
     private static void Main()
     {
-        
+
         Console.Clear();
-        Console.SetCursorPosition(50,15);
+        Console.SetCursorPosition(25,15);
         var info = @"                                   Onze applicatie werkt het best in full screen.
-                                                              Als u niet het in fullscreen heeft kunnen we niet garanderen dat de applicatie werkt als verwacht. 
-                                                                                  Druk een toets in als u dit bericht gelezen heeft.";
+                                                             Als u het niet in fullscreen heeft,
+                                                kunnen we niet garanderen dat de applicatie werkt als verwacht. 
+                                                       Druk een toets in als u dit bericht gelezen heeft.";
         Console.WriteLine(info);
         Console.ReadKey();
         Console.Clear();
