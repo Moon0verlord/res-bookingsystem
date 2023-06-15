2779e96cd870f02aaec6cb6993d59b00a25cfc7e
WarMartijn <1058740@hr.nl>
2023-06-15 13:21:16 +0200

    Made a message at the start indicating the user to increase screen size

--- Project/Program.cs
+++ Project/Program.cs
@@ -3,7 +3,14 @@ public class Program
 {
     private static void Main()
     {
-        // todo: ask user to set fullscreen here, removed old cuz of bugs
+        
+        Console.Clear();
+        Console.SetCursorPosition(50,15);
+        var info = @"                                   Onze applicatie werkt het best in full screen.
+                                                              Als u niet het in fullscreen heeft kunnen we niet garanderen dat de applicatie werkt als verwacht. 
+                                                                                  Druk een toets in als u dit bericht gelezen heeft.";
+        Console.WriteLine(info);
+        Console.ReadKey();
         Console.Clear();
         MainMenu.Start();
     }
